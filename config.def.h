52774531aa97ac94ebbc56e7c491eb4abb94bb24
Piotr Serafin <piotr.serafin.71@gmail.com>
2020-04-21 12:24:12 +0200

    Added highlight patch

--- config.def.h
+++ config.def.h
@@ -24,6 +24,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     		fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+    [SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 
